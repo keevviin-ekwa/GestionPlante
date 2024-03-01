b535034f87bcc4261fac7f6ea81f4063120aec60
Bayard Ekwa <ekwabayard@gmail.com>
2024-03-01 11:13:55 -0500

    second commit

--- app/Http/Controllers/PlanteController.php
+++ app/Http/Controllers/PlanteController.php
@@ -2,14 +2,82 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Plante;
 use Illuminate\Http\Request;
 
 class PlanteController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $plantes=Plante::findAll();
-        return $plantes;
+        $plantes=Plante::all();
+        return  $plantes;
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //dd($request->all());
+        $plante= new Plante();
+        $plante->nom=$request->nom;
+        $plante->espece=$request->espece;
+        $plante->image=$request->image;
+        $plante->created_at= new \DateTime();
+        $plante->save();
+        return $plante;
+    }
+
+    /**
+     * Affiche une plante
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $plante=Plante::find($id);
+        return $plante;
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $plante= Plante::find($id);
+        $plante->nom=$request->nom;
+        $plante->espece=$request->espece;
+        $plante->image=$request->image;
+        $plante->update();
+        return "Modification reussie";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $produit=Plante::find($id);
+        $produit->delete();
+        return "Suppression reussie";
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\PlanteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Controller\PlanteController;
 
 
 /*
@@ -15,8 +15,16 @@ use App\Controller\PlanteController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//
+//Route::get(
+//    '/plante',
+//    [PlanteController::class, 'index']
+//)->name('allPlante');
 
-Route::get(
-    '/plante',
-    [PlanteController::class, 'index']
-)->name('allPlante');
+Route::get('plante',[PlanteController::class,'index']);
+Route::get('/plante/{id}',[PlanteController::class,'show']);
+Route::delete('/plante/delete/{id}',[PlanteController::class,'destroy']);
+Route::post('/plante',[PlanteController::class,'store']);
+Route::put("/plante/{id}",[PlanteController::class,'update']);
+//Route::apiResource('plante',PlanteController::class);
+// 4 routes, get , post, put
